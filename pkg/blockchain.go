90395125920cd9e79d510f4f619c15d351b383f7
arturbaldoramos <arturbaldoramos@gmail.com>
2024-03-14 22:13:54 -0300

    feat: block structure

---
+++ pkg/blockchain.go
@@ -0,0 +1,18 @@
+package blockchain
+
+import "time"
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
