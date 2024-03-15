d00e66a522e478ac0b290872c178e2a1bdf02778
Artur Baldo Ramos <arturbaldoramos@gmail.com>
2024-03-15 13:51:48 -0300

    feat: implement all block methods

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	blockchain "github.com/arturbaldoramos/go-crypto/pkg"
+)
 
 func main() {
-	fmt.Println("Hello from Main")
+	bc := blockchain.NewBlockchain()
+
+	bc.AddTransaction("A", "B", 1.0)
+	previousHash := bc.LastBlock().Hash()
+	nonce := bc.ProofOfWork()
+	bc.CreateBlock(nonce, previousHash)
+
+	bc.AddTransaction("C", "D", 2.0)
+	bc.AddTransaction("X", "Y", 3.0)
+	previousHash = bc.LastBlock().Hash()
+	nonce = bc.ProofOfWork()
+	bc.CreateBlock(nonce, previousHash)
+
+	bc.Print()
 }
