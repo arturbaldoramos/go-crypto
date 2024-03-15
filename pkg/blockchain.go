0da098d35f050ba5b817e659dd04529c464f23e5
arturbaldoramos <arturbaldoramos@gmail.com>
2024-03-14 22:17:30 -0300

    feat: print block

--- pkg/blockchain.go
+++ pkg/blockchain.go
@@ -1,6 +1,9 @@
 package blockchain
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Block struct {
 	nonce        int
@@ -15,4 +18,11 @@ func NewBlock(nonce int, previousHash string) *Block {
 	b.nonce = nonce
 	b.previousHash = previousHash
 	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp        	  %d/n", b.timestamp)
+	fmt.Printf("nonce	        	  %d/n", b.nonce)
+	fmt.Printf("previus_hash 		  %d/n", b.previousHash)
+	fmt.Printf("transactions	          %d/n", b.transactions)
 }
